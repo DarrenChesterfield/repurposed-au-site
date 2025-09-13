c7b5b2e7ff60a60228236c1e0cb5841bd92062c9
DarrenChesterfield <darren.chesterfield@hotmail.com>
2025-09-14 09:06:04 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: false }, // force Pages Router
-};
-module.exports = nextConfig;
+experimental: { appDir: false }
