55797a862cffa5611b278416c8e38b633e0c6df0
DarrenChesterfield <darren.chesterfield@hotmail.com>
2025-09-14 09:50:23 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
-// pages/_app.js
-import '../styles/globals.css'; // This points to styles/globals.css at the root of your repo
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
