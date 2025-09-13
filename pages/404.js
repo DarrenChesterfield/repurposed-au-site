694101b2deec5dbc2c2c87423926598fc8b22547
DarrenChesterfield <darren.chesterfield@hotmail.com>
2025-09-14 09:10:17 +1000

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,6 @@
-export default function NotFound() {
-  return (
-    <main style={{minHeight:'60vh',display:'grid',placeItems:'center',fontFamily:'system-ui'}}>
-      <div style={{textAlign:'center'}}>
-        <h1 style={{fontSize:'2rem',margin:0}}>Page not found</h1>
-        <p style={{color:'#64748b'}}>Let’s get you back <a href="/">home</a>.</p>
-      </div>
-    </main>
-  );
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: { appDir: false }, // force Pages Router
+};
+module.exports = nextConfig;
