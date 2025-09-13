ff51103babbe5a39204adc40c7beb9f1d8047ffa
DarrenChesterfield <darren.chesterfield@hotmail.com>
2025-09-14 09:43:20 +1000

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: false }, // force Pages Router
-};
-module.exports = nextConfig;
+export default function Custom404() {
+  return (
+    <div style={{textAlign: 'center', padding: '50px'}}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you’re looking for does not exist.</p>
+    </div>
+  );
+}
